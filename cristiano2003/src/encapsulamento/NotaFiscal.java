0d8f234354844032eb99944f8095089563f3e32e
allankanzler <paraibakan@gmail.com>
2015-03-25 15:17:18 -0300

    Update NotaFiscal.java

--- cristiano2003/src/encapsulamento/NotaFiscal.java
+++ cristiano2003/src/encapsulamento/NotaFiscal.java
@@ -10,7 +10,7 @@ public class NotaFiscal {
 	private Produto[] produtos;
 
 	public NotaFiscal(int numero, Data data, Vendedor vendedor) {
-		produtos = new Produto[3];
+		produtos = new Produto[10];
 		this.numero = numero;
 		this.data = data;
 		this.vendedor = vendedor;
@@ -30,7 +30,7 @@ public class NotaFiscal {
 	
 	private int lugarVazio(){
 		int lugar = -1;
-		for(int i = 0; i<3; i++){
+		for(int i = 0; i<10; i++){
 			if(produtos[i]==null){
 				lugar = i;
 				break;
@@ -42,7 +42,7 @@ public class NotaFiscal {
 	
 	private boolean produtoJaExiste(Produto produto) {
 		Produto temporario = null;
-		for(int i = 0; i< 3; i++){
+		for(int i = 0; i< 10; i++){
 			temporario = produtos[i];
 			if(temporario!=null)
 				if(temporario.getCodigo()==produto.getCodigo())
@@ -54,7 +54,7 @@ public class NotaFiscal {
 	
 	private boolean existeEspacoNoVetor() {
 		
-		for(int i = 0; i< 3; i++){
+		for(int i = 0; i< 10; i++){
 			if(produtos[i]==null)
 				return true;
 		}
@@ -64,7 +64,7 @@ public class NotaFiscal {
 	
 	public double somarProdutos(){
 		double soma = 0;
-		for(int i =0; i< 3;i++)
+		for(int i =0; i< 10;i++)
 			if (produtos[i] == null)
 				break;
 			else
@@ -73,119 +73,4 @@ public class NotaFiscal {
 		return soma;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	private int numero;
-	private String data;
-	
-	private Vendedor vendedor; //nome,cpf
-	private Produto[] produtos = new Produto[10]; //10 no máximo
-	
-	
-	//construtor
-	
-	public NotaFiscal(int numero, String data, Vendedor vendedor){
-		this.numero = numero;
-		this.data = data;
-		this.vendedor = vendedor;
-	}
-	
-	//getters and setters
-	public Produto[] getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(Produto[] produtos) {
-		this.produtos = produtos;
-	}
-	
-	public int getNumero() {
-		return numero;
-	}
-
-	public String getData() {
-		return data;
-	}
-
-	public Vendedor getVendedor() {
-		return vendedor;
-	}
-	
-
-	//metodo adicionar produtos, faz a verificação pelo codigo se o
-	//produto já existe. Estre metodo deve retornar um booleano
-	
-	public boolean adicionarProdutos (Produto produto){
-		int i=0;
-				
-		if(this.existeEspacoNoVetor(produto)){
-			if (this.verificarProduto(produto)==false){
-				for (i=0;i<10;i++){
-					if (produtos[i] ==null){
-						produtos[i] = produto;
-						return true;
-					}						
-				}
-			}
-		}			
-		return false;
-		
-	}
-	
-	
-	private boolean existeEspacoNoVetor(Produto produto){
-		
-		for (int i=0;i<10;i++){
-			if (this.produtos[i]==null)
-				return true;
-		}
-		return false;
-	}
-		
-	private boolean verificarProduto(Produto produto){
-		boolean naoRepetido = false;
-		String temp;
-		
-		for (int i=0;i<10;i++){
-			
-			if (this.produtos[i]==null)
-				temp = "0";
-			else
-				temp = this.produtos[i].getCodigo();
-			
-			if(temp.equals(produto.getCodigo()) ){
-			naoRepetido = true;
-			}
-		}	
-			
-		return naoRepetido;
-	}
-		
-	
-	//metodo chamado calcularTotal que retorna o valor total da nota
-	
-	public double calcularValor(){
-		double total=0;
-		
-		for(int i=0;i<10;i++)
-			if (this.produtos[i]!=null)
-				total += this.produtos[i].getPreco();
-			else
-				break;
-		return total;
-	}
-	*/
-
 }
